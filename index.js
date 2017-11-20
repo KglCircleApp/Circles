21ec05b940e3c48a303a2ea9fdc50cf08cf9cadb
ishimwe <ishimwefelix@gmail.com>
2017-11-20 05:24:18 +0200

    feeds section

--- index.js
+++ index.js
@@ -3,32 +3,36 @@ import { StackNavigator } from 'react-navigation';
 
 import LoginScreen from './app/screen/Login';
 import RegisterScreen from './app/screen/Register';
-import VerifyScreen    from  './app/screen/Verify';
-import HomeScreen      from  './app/screen/Home';
-import ProfileScreen    from './app/screen/Profile';
+import VerifyScreen from './app/screen/Verify';
+import HomeScreen from './app/screen/Home';
+import ProfileScreen from './app/screen/Profile';
+import FeedScreen from './app/screen/Feeds';
 
 export const App = StackNavigator({
-  
-  Login : { 
-    screen : LoginScreen
-  },
-  Register : { 
-    screen : RegisterScreen 
-  },
-  Verify   : {
-    screen : VerifyScreen
-  },
-  Home : {
-    screen : HomeScreen
-  },
-  Profile : {
-    screen : ProfileScreen
-  }
-},
 
-{
-initialRouteName: 'Login',
-}
+        Login: {
+            screen: LoginScreen
+        },
+        Register: {
+            screen: RegisterScreen
+        },
+        Verify: {
+            screen: VerifyScreen
+        },
+        Home: {
+            screen: HomeScreen
+        },
+        Profile: {
+            screen: ProfileScreen
+        },
+        Feeds: {
+            screen: FeedScreen
+        },
+    },
+
+    {
+        initialRouteName: 'Login',
+    }
 );
 
 AppRegistry.registerComponent('Circles', () => App);
